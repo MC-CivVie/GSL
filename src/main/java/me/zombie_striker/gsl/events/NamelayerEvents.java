8aba9fe44727f90268c8d4aed5c33ac94c80eead
ZombieStriker <dannyboyhn@yahoo.com>
2023-02-04 09:10:22 -0500

    Fixed namelayer on join

--- src/main/java/me/zombie_striker/gsl/events/NamelayerEvents.java
+++ src/main/java/me/zombie_striker/gsl/events/NamelayerEvents.java
@@ -1,6 +1,7 @@
 package me.zombie_striker.gsl.events;
 
 import me.zombie_striker.gsl.namelayers.NameLayer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -12,6 +13,7 @@ public class NamelayerEvents implements @NotNull Listener {
     public void onJoin(PlayerJoinEvent event){
         if(!event.getPlayer().hasPlayedBefore()){
             NameLayer nameLayer = new NameLayer(event.getPlayer().getName(),event.getPlayer().getUniqueId());
+            nameLayer.addMemberRank(event.getPlayer().getUniqueId(),(byte)0);
             NameLayer.register(nameLayer);
         }
     }
